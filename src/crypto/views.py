7c678df802a54f8355ddc9cb19d95d571415ee4c
Adrianacmy <iamadrianachen@gmail.com>
2018-07-13 14:56:02 -0700

    test price API

--- src/crypto/views.py
+++ src/crypto/views.py
@@ -8,6 +8,16 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-  url = 'https://min-api.cryptocompare.com/data/v2/news/?lang=EN'
-  news = json.loads(requests.get(url).content)
-  return render(request, 'home.html', {'news': news})
+  url_news = 'https://min-api.cryptocompare.com/data/v2/news/?lang=EN'
+  news = json.loads(requests.get(url_news).content)
+
+  url_price = 'https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,ETH,EOS,LTC,XRP&tsyms=USD,EUR'
+  price = json.loads(requests.get(url_price).content)
+
+  context = {
+    'news': news,
+    'price': price
+  }
+
+  return render(request, 'home.html', context)
+
