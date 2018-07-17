a55cb1cb3132e060c4977fe50f3e32870ec24280
Adrianacmy <iamadrianachen@gmail.com>
2018-07-16 20:24:31 -0700

    update homepage url

--- src/products/models.py
+++ src/products/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Product(models.Model):
   title = models.CharField(max_length=255)
-  description = models.TextField()
+  description = models.TextField(blank=True, null=True)
   price = models.DecimalField(max_digits=100, decimal_places=2)
 
   def __str__(self):
